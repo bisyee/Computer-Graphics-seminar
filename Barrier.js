065c847e62d9bb987015f170cba009cbfc181b3f
KCerin <cerinklemen@gmail.com>
2023-01-02 19:08:00 +0100

    Barrier

---
+++ Barrier.js
@@ -0,0 +1,30 @@
+const vec3 = glMatrix.vec3;
+
+export default class Barrier {
+     constructor() {   
+        this.barrier = null;
+    }
+
+    async build(loader) {
+        await loader.load('./blender/map/city.gltf');
+        let b = await loader.loadNode('barrier');
+        b.translation = vec3.set(vec3.create(), 0, 400, 0);
+        b.scale = vec3.set(vec3.create(), 1500, 1500, 1500);
+        this.barrier = b;
+        //console.log(b);
+    }
+    
+    collision(object) {
+        let collided = false;
+        if ((object.translation[0] > this.barrier.translation[0]+1500 ||
+            object.translation[0] < this.barrier.translation[0]-1500 ||
+            object.translation[2] > this.barrier.translation[2]+1500 || 
+            object.translation[2] < this.barrier.translation[2]-1500
+            )) {
+            console.log('hit');
+            collided = true;
+        }          
+        return collided;
+    }
+
+}
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { PerspectiveCamera } from './PerspectiveCamera.js';
 import { GLTFLoader } from './GLTFLoader.js';
 import { Renderer } from './Renderer.js';
 import { FirstPersonController } from './FirstPersonController.js';
+import Barrier from './Barrier.js';
 import Coins from './Coins.js';
 import Meteors from './Meteors.js';
 import Fuel from './Fuel.js';
@@ -43,6 +44,9 @@ class App extends Application {
         if (!this.camera.camera) {
             throw new Error('Camera node does not contain a camera reference');
         }   
+
+        this.barrier = new Barrier();
+        await this.barrier.build(this.loader);
 
         this.coins = new Coins();
         await this.coins.build(this.loader,  this.scene);
@@ -91,6 +95,8 @@ class App extends Application {
             roll             : 0,
             collided         : false
         });
+
+        this.planeT = [this.plane.translation[0], this.plane.translation[1], this.plane.translation[2]];
         
         this.scene.addNode(this.plane);
         this.scene.addNode(this.camera);
@@ -122,7 +128,15 @@ class App extends Application {
         this.controller.update(dt);
         var collidedCoins = this.coins.collisionCoins(this.plane, this.scene);
         let angles = this.getEuler(this.plane.rotation);
-        var collided = this.meteors.collision(this.plane,angles);
+        var collided = this.meteors.collision(this.plane, angles);
+        //console.log(this.plane.translation);
+
+        var collidedBarrier = this.barrier.collision(this.plane, angles);
+
+        if (collidedBarrier){
+            this.plane.translation = vec3.set(vec3.create(), this.planeT[0], 400, this.planeT[2]);
+            //this.life.subLife();
+        }
         
         if (collided){
             this.plane.translation = vec3.set(vec3.create(), this.plane.translation[0] - 20, 400, this.plane.translation[2] - 20);
@@ -136,7 +150,7 @@ class App extends Application {
         // if(this.plane.translation[0] == vec3.set(vec3.create(), 720, 200, 900)[0]){
         //     console.log('star');
         // }
-
+        this.planeT = [this.plane.translation[0], this.plane.translation[1], this.plane.translation[2]];
     }
 
     getEuler(q) {
